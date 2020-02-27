2d1b84a5287f612a261ff8073058b20f5ddc03e8
vinsmoke <bharathkaranth07@gmail.com>
2020-02-27 16:31:48 +0530

    Added a disconnect event to toggle isActive

--- server/socketMain.js
+++ server/socketMain.js
@@ -9,12 +9,25 @@ const socketMain = (io, socket) => {
             socket.join('clients');
         } else if (key === 'akjsdkjk232asDDS') {
             socket.join('ui');
-            console.log('A react app joined')
+            console.log('A react app client joined');
+            Machine.find({}, (err, docs) => {
+                docs.forEach(aMachine => {
+                    aMachine.isActive = false;
+                    io.to('ui').emit('data', aMachine);
+                })
+            });
         } else {
             socket.disconnect(true);
         }
     });
-
+    socket.on('disconnect', () => {
+        Machine.find({ macA }, (err, docs) => {
+            if (docs.length > 0) {
+                docs[0].isActive = false;
+                io.to('ui').emit('data', docs[0])
+            }
+        })
+    })
     socket.on('initPerfData', (async (data) => {
         macA = data.macA;
         const mongooseResponse = await checkAndAdd(data);
